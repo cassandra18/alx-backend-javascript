ae79f61a70a8559ec91a2df39ccd30f07e6a81dd
cassandra18 <cassandralelei013@gmail.com>
2024-08-07 21:03:28 +0300

    removed eslint errors

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,9 +1,8 @@
 function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-      // You can add some logic here if needed
-      resolve('API response');
-    });
-  }
-  
-  export default getResponseFromAPI;
-  
+  return new Promise((resolve, reject) => {
+    // You can add some logic here if needed
+    resolve("API response");
+  });
+}
+
+export default getResponseFromAPI;
--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,21 +1,19 @@
 import signUpUser from "./4-user-promise";
 import uploadPhoto from "./5-photo-reject";
 
-
 function handleProfileSignup(firstName, lastName, fileName) {
-    const userPromise = signUpUser(firstName, lastName);
-    const photoPromise = uploadPhoto(fileName);
-  
-    return Promise.allSettled([userPromise, photoPromise])
-      .then((results) => {
-        return results.map((result) => {
-          if (result.status === 'fulfilled') {
-            return { status: 'fulfilled', value: result.value };
-          } else {
-            return { status: 'rejected', value: result.reason };
-          }
-        });
-      });
-  }
-  
-  export default handleProfileSignup;
+  const userPromise = signUpUser(firstName, lastName);
+  const photoPromise = uploadPhoto(fileName);
+
+  return Promise.allSettled([userPromise, photoPromise]).then((results) => {
+    return results.map((result) => {
+      if (result.status === "fulfilled") {
+        return { status: "fulfilled", value: result.value };
+      } else {
+        return { status: "rejected", value: result.reason };
+      }
+    });
+  });
+}
+
+export default handleProfileSignup;
