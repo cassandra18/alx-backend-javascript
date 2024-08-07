3a0fec8be3f8471bc50ed16641ebf9d28a16f21c
cassandra18 <cassandralelei013@gmail.com>
2024-08-07 21:00:05 +0300

    0x01-ES6_promise

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,9 @@
+function getResponseFromAPI() {
+    return new Promise((resolve, reject) => {
+      // You can add some logic here if needed
+      resolve('API response');
+    });
+  }
+  
+  export default getResponseFromAPI;
+  
