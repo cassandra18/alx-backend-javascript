9533ae7de42d1de367c772a763f5170253f672f6
cassandra18 <cassandralelei013@gmail.com>
2024-08-09 14:12:32 +0300

    fixed task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,18 +1,13 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-async function handleProfileSignup(firstName, lastName, fileName) {
-  const userPromise = signUpUser(firstName, lastName);
-  const photoPromise = uploadPhoto(fileName);
-
-  const results = await Promise.allSettled([userPromise, photoPromise]);
-
-  return results.map((result) => {
-    if (result.status === 'fulfilled') {
-      return { status: 'fulfilled', value: result.value };
-    }
-    return { status: 'rejected', value: result.reason };
-  });
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((res) => (
+      res.map((o) => ({
+        status: o.status,
+        value: o.status === 'fulfilled' ? o.value : String(o.reason),
+      }))
+    ));
 }
-
-export default handleProfileSignup;
