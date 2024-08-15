6f7399c53b70bdec633d233b30a0041348df11d1
cassandra18 <cassandralelei013@gmail.com>
2024-08-15 06:20:04 +0300

    task to

--- 0x04-TypeScript/task_4/js/subjects/Teacher.ts
+++ 0x04-TypeScript/task_4/js/subjects/Teacher.ts
@@ -2,5 +2,8 @@ namespace Subjects {
   export interface Teacher {
     firstName: string;
     lastName: string;
+    experienceTeachingC?: number;
+    experienceTeachingJava?: number;
+    experienceTeachingReact?: number;
   }
 }
